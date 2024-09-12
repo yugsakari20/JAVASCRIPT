404342fba1c43c991d0ccc336de8a52a83e81102
yugsakari20 <yugsakariya976@gmail.com>
2024-09-12 15:59:12 +0530

    10/09/2024

--- project/js/addProduct.js
+++ project/js/addProduct.js
@@ -1,10 +1,10 @@
 import getValue from "../components/helper.js";
 import Navbar from "../components/navbar.js";
 
-// Render Navbar
+
 document.getElementById("navbar").innerHTML = Navbar();
 
-// Redirect to login if not logged in
+
 let login = localStorage.getItem("login") || false;
 if (login === "false" || login === null) {
   window.location.href = "/pages/login.html";
@@ -12,7 +12,6 @@ if (login === "false" || login === null) {
 
 let products = JSON.parse(localStorage.getItem("products")) || [];
 
-// Handle form submission
 const handleSubmit = (e) => {
   e.preventDefault();
 
@@ -28,5 +27,5 @@ const handleSubmit = (e) => {
   localStorage.setItem("products", JSON.stringify(products));
 };
 
-// Add event listener to the form
+
 document.getElementById("productData").addEventListener("submit", handleSubmit);
